bf7f9f9dcdfa187f8e595f1f0b466c114523ba4b
nezlobnaya <vlad.burlutskiy@icloud.com>
2022-05-20 15:16:24 -0400

    more tests for GetStatus func added; test passing

--- application.go
+++ application.go
@@ -32,6 +32,7 @@ func (test Application) GetStatus() *ApplicationStatus {
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
+		Timeout: test.Timeout,
 	}
 
 	resp, err := client.Head(test.URL)
