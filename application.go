f89a6e69abd5aabf7eb049eba31c8641b092a842
nezlobnaya <vlad.burlutskiy@icloud.com>
2022-05-23 14:35:35 -0400

    added tests to support checking redirect, added GetStatus support to check redirect; tests passing

--- application.go
+++ application.go
@@ -11,11 +11,12 @@ type Application struct {
 	URL                string
 	ExpectedStatusCode int
 	Timeout            time.Duration
+	ExpectedLocation   string `default:""` 
 }
 
 // NewApplication returns a Application initialized with specified values
-func NewApplication(url string, expectedStatusCode int, timeout time.Duration) *Application {
-	return &Application{url, expectedStatusCode, timeout}
+func NewApplication(url string, expectedStatusCode int, timeout time.Duration, expectedLocation string) *Application {
+	return &Application{url, expectedStatusCode, timeout, expectedLocation}
 }
 
 // ApplicationStatus represents the results of a synthetic test
@@ -23,6 +24,7 @@ type ApplicationStatus struct {
 	Application      *Application
 	Success          bool
 	ActualStatusCode int
+	ActualLocation   string `default:""`
 }
 
 // GetStatus performs an HTTP call for the given Application's url and returns the ApplicationStatus corresponding to those results
@@ -37,22 +39,23 @@ func (test Application) GetStatus() *ApplicationStatus {
 
 	resp, err := client.Head(test.URL)
 	if err != nil {
-		return &ApplicationStatus{&test, false, 0}
+		return &ApplicationStatus{&test, false, 0, ""}
 	}
 
 	if resp.StatusCode != test.ExpectedStatusCode {
-		return &ApplicationStatus{&test, false, resp.StatusCode}
+		return &ApplicationStatus{&test, false, resp.StatusCode, resp.Header.Get("Location")}
 	}
 
-	return &ApplicationStatus{&test, true, resp.StatusCode}
+	return &ApplicationStatus{&test, true, resp.StatusCode, resp.Header.Get("Location")}
 }
+
 
 // String outputs the application status as a single string
 func (results ApplicationStatus) String() string {
 
 	if results.Success {
-		return fmt.Sprintf("Success: URL %s resolved with %d", results.Application.URL, results.ActualStatusCode)
+		return fmt.Sprintf("Success: URL %s resolved with %d, %s", results.Application.URL, results.ActualStatusCode, results.ActualLocation )
 	}
 
-	return fmt.Sprintf("Failure: URL %s resolved with %d, expected %d", results.Application.URL, results.ActualStatusCode, results.Application.ExpectedStatusCode)
+	return fmt.Sprintf("Failure: URL %s resolved with %d, expected %d, %s", results.Application.URL, results.ActualStatusCode, results.Application.ExpectedStatusCode, results.ActualLocation)
 }
