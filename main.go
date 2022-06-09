13d7627e560e9e517cb69ca1a592e8587882b2ba
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-06-08 20:13:10 -0400

    refactored main.go

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/NYULibraries/aswa/lib/config"
 )
 
+const yamlPath = "./config/applications.yml"
+
+func extractValuesFromConfig(app *config.Application) (url string, expectedStatusCode int, timeout time.Duration, expectedActualLocation string) {
+	url = app.URL
+	expectedStatusCode = app.ExpectedStatusCode
+	timeout = app.Timeout
+	expectedActualLocation = app.ExpectedLocation
+	return
+}
+
 func main() {
 
-	applications, err := config.NewConfig("./config/applications.yml")
+	applications, err := config.NewConfig(yamlPath)
 	if err != nil {
 		log.Println("Could not load config file; aborting!")
 		panic(err)
 	}
 
 	for _, app := range applications.Applications {
-		url := app.URL
-		expectedStatusCode := app.ExpectedStatusCode
-		timeout := app.Timeout
-		expectedActualLocation := app.ExpectedLocation
+		url, expectedStatusCode, timeout, expectedActualLocation := extractValuesFromConfig(app)
 
 		test := NewApplication(url, expectedStatusCode, timeout, expectedActualLocation)
 		appStatus := test.GetStatus()
