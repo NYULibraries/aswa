6aecdea2a8f59f9fd973ae086dbe12e25e33e311
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-06-22 14:14:08 -0400

    recommitting changes

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"os"
 
-	"github.com/NYULibraries/aswa/lib/application"
-	"github.com/NYULibraries/aswa/lib/config"
+	a "github.com/NYULibraries/aswa/lib/application"
+	c "github.com/NYULibraries/aswa/lib/config"
 )
 
 const yamlPath = "./config/applications.yml"
@@ -18,23 +18,25 @@ func main() {
 
 	cmdArg := os.Args[1] // get the command line argument
 
-	applications, err := config.NewConfig(yamlPath)
+	inputData, err := c.NewConfig(yamlPath)
+
+	appData := inputData.Applications
 
 	if err != nil {
 		log.Println("Could not load config file; aborting!")
 		panic(err)
 	}
 
-	if !config.ContainApp(applications.Applications, cmdArg) {
+	if !c.ContainApp(appData, cmdArg) {
 		log.Println("Application '", cmdArg, "' not found in config file; aborting!")
 		panic(err)
 	}
 
-	for _, app := range applications.Applications {
-		name, url, expectedStatusCode, timeout, expectedActualLocation := config.ExtractValuesFromConfig(app)
+	for _, app := range appData {
+		name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
 
 		if cmdArg == name {
-			test := application.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
+			test := a.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
 			appStatus := test.GetStatus()
 			log.Println(appStatus)
 		}
