1093e7f52b69dbcb7dfa8b2824fd8bae039b3b77
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-07-07 13:07:58 -0400

    added an option to run tests without args

--- main.go
+++ main.go
@@ -12,12 +12,6 @@ const yamlPath = "./config/applications.yml"
 
 func main() {
 
-	if len(os.Args) == 1 {
-		panic("No application name provided")
-	}
-
-	cmdArg := os.Args[1] // get the command line argument
-
 	inputData, err := c.NewConfig(yamlPath)
 
 	if err != nil {
@@ -27,23 +21,39 @@ func main() {
 
 	appData := inputData.Applications
 
-	if !c.ContainApp(appData, cmdArg) {
-		log.Println("Application '", cmdArg, "' not found in config file; aborting!")
-		panic(err)
-	}
+	if len(os.Args) == 1 {
+		for _, app := range appData {
+			name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
 
-	for _, app := range appData {
-		name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
-
-		if cmdArg == name {
 			test := a.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
 			appStatus := test.GetStatus()
 			log.Println(appStatus)
 
 			PostToSlack(appStatus.String())
-
-			break
 		}
 
+	} else {
+
+		cmdArg := os.Args[1] // get the command line argument
+
+		if !c.ContainApp(appData, cmdArg) {
+			log.Println("Application '", cmdArg, "' not found in config file; aborting!")
+			panic(err)
+		}
+
+		for _, app := range appData {
+			name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
+
+			if cmdArg == name {
+				test := a.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
+				appStatus := test.GetStatus()
+				log.Println(appStatus)
+
+				PostToSlack(appStatus.String())
+
+				break
+			}
+
+		}
 	}
 }
