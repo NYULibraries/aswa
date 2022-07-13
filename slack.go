332599f49d71d7eef0a5137fff703fa8a1756e14
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-07-13 09:43:27 -0400

    refactoring timestamp in slack.go

--- slack.go
+++ slack.go
@@ -4,22 +4,8 @@ import (
 	"github.com/slack-go/slack"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
-
-func parseUnixTimestamp(ms string) (time.Time, error) {
-	str := strings.Split(ms, ".")[0]
-	i, err := strconv.ParseInt(str, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	tm := time.Unix(i, 0)
-
-	return tm, nil
-}
 
 func PostToSlack(status string) {
 	token := os.Getenv("SLACK_TOKEN")
@@ -38,14 +24,14 @@ func PostToSlack(status string) {
 
 	api := slack.New(token)
 
-	channelID, timestamp, err := api.PostMessage(channel, slack.MsgOptionText(status, false))
+	channelID, _, err := api.PostMessage(channel, slack.MsgOptionText(status, false))
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	parsed_time, _ := parseUnixTimestamp(timestamp)
+	timestamp := time.Now().Local().Format(time.ANSIC)
 
-	log.Printf("Message sent to channel %s at %s", channelID, parsed_time)
+	log.Printf("Message sent to channel %s on %s", channelID, timestamp)
 }
