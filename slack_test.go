7f288763341437b55f8c4c495f5e7a1747c05953
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-08-17 14:20:39 -0400

    slack_test.go refactored

--- slack_test.go
+++ slack_test.go
@@ -17,40 +17,41 @@ func (m *mockPostMessageClient) PostMessage(channel string, options ...slack.Msg
 	return m.mockChannelID, m.mockStatus, m.mockError
 }
 
-func testPostToSlackWithClientFunc(t *testing.T, channelID string, status string, error error) {
+func testPostToSlackFunc(t *testing.T, channelID string, status string, error error) {
+	mockClient := &mockPostMessageClient{
+		mockChannelID: channelID,
+		mockStatus:    status,
+		mockError:     error,
+	}
+	slackClient := &SlackClient{
+		api: mockClient,
+	}
+	slackClient.PostToSlack(status)
 
-	mockApi := &mockPostMessageClient{channelID, status, error}
-	PostToSlackWithClient(status, mockApi)
-
-	if assert.Equal(t, channelID, mockApi.mockChannelID) {
-		assert.True(t, true)
-
-	} else {
-		assert.True(t, false)
+	if assert.Equal(t, channelID, mockClient.mockChannelID) {
+		assert.Equal(t, status, mockClient.mockStatus)
+		assert.Equal(t, error, mockClient.mockError)
 	}
 
-	if assert.Equal(t, status, mockApi.mockStatus) {
-		assert.True(t, true)
-	} else {
-		assert.True(t, false)
+	if assert.NotNil(t, slackClient) {
+		assert.NotNil(t, slackClient.api)
 	}
 
-	if assert.Equal(t, error, mockApi.mockError) {
-		assert.True(t, true)
-	} else {
-		assert.True(t, false)
+	if assert.NotNil(t, slackClient.api) {
+		assert.Equal(t, mockClient, slackClient.api)
 	}
+
 }
 
-func TestPostToSlackWithClient(t *testing.T) {
+func TestPostToSlack(t *testing.T) {
 	var tests = []struct {
 		description string
 		channelID   string
 		status      string
 		error       error
 	}{
-		{"Valid channelID and status", "C1234567890", "testStatus", nil},
-		{"Invalid channelID", "", "testStatus", nil},
+		{"Valid channelID and status", "C1234567890", "Status", nil},
+		{"Invalid channelID", "", "Status", nil},
 		{"Invalid status", "C1234567890", "", nil},
 		{"Invalid channelID and status", "", "", nil},
 		{"Invalid channelID and status and error", "", "", error(nil)},
@@ -58,7 +59,53 @@ func TestPostToSlackWithClient(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			testPostToSlackWithClientFunc(t, test.channelID, test.status, test.error)
+			testPostToSlackFunc(t, test.channelID, test.status, test.error)
 		})
 	}
 }
+
+// func testPostToSlackFunc(t *testing.T, channelID string, status string, error error) {
+
+// 	mockApi := &mockPostMessageClient{channelID, status, error}
+// 	PostToSlack(status)
+
+// 	if assert.Equal(t, channelID, mockApi.mockChannelID) {
+// 		assert.True(t, true)
+
+// 	} else {
+// 		assert.True(t, false)
+// 	}
+
+// 	if assert.Equal(t, status, mockApi.mockStatus) {
+// 		assert.True(t, true)
+// 	} else {
+// 		assert.True(t, false)
+// 	}
+
+// 	if assert.Equal(t, error, mockApi.mockError) {
+// 		assert.True(t, true)
+// 	} else {
+// 		assert.True(t, false)
+// 	}
+// }
+
+// func TestPostToSlack(t *testing.T) {
+// 	var tests = []struct {
+// 		description string
+// 		channelID   string
+// 		status      string
+// 		error       error
+// 	}{
+// 		{"Valid channelID and status", "C1234567890", "testStatus", nil},
+// 		{"Invalid channelID", "", "testStatus", nil},
+// 		{"Invalid status", "C1234567890", "", nil},
+// 		{"Invalid channelID and status", "", "", nil},
+// 		{"Invalid channelID and status and error", "", "", error(nil)},
+// 	}
+
+// 	for _, test := range tests {
+// 		t.Run(test.description, func(t *testing.T) {
+// 			testPostToSlackFunc(t, test.channelID, test.status, test.error)
+// 		})
+// 	}
+// }
