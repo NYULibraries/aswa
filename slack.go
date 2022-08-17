f4fc7717669fef1be9d1be518415e9ca1ad880ff
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-08-17 13:52:14 -0400

    SlackClient struct added,slack.go refactored to use the struct

--- slack.go
+++ slack.go
@@ -11,31 +11,24 @@ type postMessageClient interface {
 	PostMessage(channel string, options ...slack.MsgOption) (string, string, error)
 }
 
-func PostToSlack(status string) {
-
-	token := os.Getenv("SLACK_TOKEN")
-
-	if token == "" {
-		log.Println("SLACK_TOKEN not set; aborting posting slack message!")
-		return
-	}
-
-	api := slack.New(token)
-
-	PostToSlackWithClient(status, api)
-
+type SlackClient struct {
+	api postMessageClient
 }
 
-func PostToSlackWithClient(status string, client postMessageClient) {
+func NewSlackClient(token string) *SlackClient {
+	api := slack.New(token)
+	return &SlackClient{api}
+}
 
+func (s *SlackClient) PostToSlack(status string) {
 	channel := os.Getenv("SLACK_CHANNEL_ID")
 
 	if channel == "" {
-		log.Println("SLACK_CHANNEL _ID not set; aborting posting slack message!")
+		log.Println("SLACK_CHANNEL_ID not set; aborting posting slack message!")
 		return
 	}
 
-	channelID, _, err := client.PostMessage(channel, slack.MsgOptionText(status, false))
+	channelID, _, err := s.api.PostMessage(channel, slack.MsgOptionText(status, false))
 
 	if err != nil {
 		log.Fatal(err)
@@ -45,5 +38,4 @@ func PostToSlackWithClient(status string, client postMessageClient) {
 	timestamp := time.Now().Local().Format(time.ANSIC)
 
 	log.Printf("Message sent to channel %s on %s", channelID, timestamp)
-
 }
