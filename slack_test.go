ee6a44771648f44b7a8709ad7ceb747ff22e8701
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-09-01 16:12:32 -0400

    slack_test.go:added spies to mock client,refactored PostToSlack test funcs

--- slack_test.go
+++ slack_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/slack-go/slack"
@@ -25,26 +26,32 @@ type mockPostMessageClient struct {
 	mockChannelID string
 	mockStatus    string
 	mockError     error
+	//these are the spies that will be used to verify the arguments passed to the PostMessage method
+	channelArg string
+	optionsArg []slack.MsgOption
 }
 
 func (m *mockPostMessageClient) PostMessage(channel string, options ...slack.MsgOption) (string, string, error) {
+	m.channelArg = channel
+	m.optionsArg = options
 	return m.mockChannelID, m.mockStatus, m.mockError
 }
 
-func testPostToSlackFunc(t *testing.T, channelID string, status string, error error) {
+func testPostToSlackFunc(t *testing.T, channelID string, status string, mockError error) {
 	mockClient := &mockPostMessageClient{
 		mockChannelID: channelID,
 		mockStatus:    status,
-		mockError:     error,
+		mockError:     mockError,
 	}
 	slackClient := &SlackClient{
 		api: mockClient,
 	}
-	slackClient.PostToSlack(status, channelID)
 
-	if assert.Equal(t, channelID, mockClient.mockChannelID) {
-		assert.Equal(t, status, mockClient.mockStatus)
-		assert.Equal(t, error, mockClient.mockError)
+	err := slackClient.PostToSlack(status, channelID)
+
+	if assert.Equal(t, channelID, mockClient.channelArg) {
+		// assert.Equal(t, slack.MsgOptionText(status, false), mockClient.optionsArg)
+		assert.Equal(t, err, mockClient.mockError)
 	}
 
 	if assert.NotNil(t, slackClient) {
@@ -68,7 +75,7 @@ func TestPostToSlack(t *testing.T) {
 		{"Invalid channelID", "", "Status", nil},
 		{"Invalid status", "C1234567890", "", nil},
 		{"Invalid channelID and status", "", "", nil},
-		{"Invalid channelID and status and error", "", "", error(nil)},
+		{"Invalid channelID and status and error", "", "", errors.New("Slack is down!")},
 	}
 
 	for _, test := range tests {
