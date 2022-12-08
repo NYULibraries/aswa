24ec5c93855557bd34ae459c5a182739f2114322
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-12-07 23:17:24 -0500

    helper.go: added checkSlackEnvs function; RunTests func refactored

--- helper.go
+++ helper.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	c "github.com/NYULibraries/aswa/lib/config"
 	"log"
+	"os"
 
 	a "github.com/NYULibraries/aswa/lib/application"
-	c "github.com/NYULibraries/aswa/lib/config"
 )
 
 func postTestResult(test *a.Application, channel string, token string) error {
@@ -20,40 +23,42 @@ func postTestResult(test *a.Application, channel string, token string) error {
 	return nil
 }
 
-func RunTestsNoCmdArgs(appData []*c.Application, channel string, token string) error {
-
-	for _, app := range appData {
-		name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
-
-		test := a.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
-		err := postTestResult(test, channel, token)
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-	}
-	return nil
-}
-
 func RunTests(appData []*c.Application, channel string, token string, cmdArg string) error {
-
-	if !c.ContainApp(appData, cmdArg) {
-		log.Println("Application '", cmdArg, "' not found in config file; aborting!")
-	}
-
+	found := false // Keep track of whether the app was found in the config file
 	for _, app := range appData {
 		name, url, expectedStatusCode, timeout, expectedActualLocation := c.ExtractValuesFromConfig(app)
 
-		if cmdArg == name {
+		if cmdArg == "" || cmdArg == name {
+			found = true // The app was found in the config file
 			test := a.NewApplication(name, url, expectedStatusCode, timeout, expectedActualLocation)
 			err := postTestResult(test, channel, token)
 			if err != nil {
 				log.Println(err)
 				return err
 			}
-			break
-		}
 
+			if cmdArg != "" {
+				break
+			}
+		}
+	}
+	if !found && cmdArg != "" {
+		// The app was not found in the config file
+		err := fmt.Errorf("app '%s' not found in config file", cmdArg)
+		log.Println(err)
+		return err
 	}
 	return nil
+}
+
+func checkSlackEnvs() (string, string, error) {
+	if os.Getenv("SLACK_CHANNEL_ID") == "" {
+		err := errors.New("SLACK_CHANNEL_ID environment variable is not set")
+		log.Fatal("Error checking Slack environment variable SLACK_CHANNEL_ID:", err)
+	}
+	if os.Getenv("SLACK_TOKEN") == "" {
+		err := errors.New("SLACK_TOKEN environment variable is not set")
+		log.Fatal("Error checking Slack environment variable SLACK_TOKEN:", err)
+	}
+	return os.Getenv("SLACK_CHANNEL_ID"), os.Getenv("SLACK_TOKEN"), nil
 }
