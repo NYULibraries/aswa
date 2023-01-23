7903eabe92f1b9f27e6e5cacdc2ac0539344b499
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-01-23 12:24:23 -0500

    lib/config/config.go: func anyRequiredEmpty renamed, unused code removed

--- lib/config/config.go
+++ lib/config/config.go
@@ -2,29 +2,25 @@ package config
 
 import (
 	"errors"
-	"os"
-	"time"
-
 	a "github.com/NYULibraries/aswa/lib/application"
 	"gopkg.in/yaml.v3"
+	"os"
 )
 
 // Config struct to replace environment variables
 type Config struct {
-	Applications []*Application
+	Applications []*a.Application
 }
 
-type Application a.Application
-
 // Check if any required App field is empty
-func (app *Application) hasEmptyRequiredFields() bool {
+func hasEmptyRequiredFields(app *a.Application) bool {
 	return app.Name == "" || app.URL == "" || app.ExpectedStatusCode == 0
 }
 
 // Loop through all applications and check if any required field is empty
-func (list *Config) anyRequiredEmpty() bool {
+func (list *Config) isConfigAnyRequiredFieldEmpty() bool {
 	for _, app := range list.Applications {
-		if app.hasEmptyRequiredFields() {
+		if hasEmptyRequiredFields((*a.Application)(app)) {
 			return true
 		}
 	}
@@ -42,23 +38,14 @@ func loadConfig(yamlPath string) (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	if config.anyRequiredEmpty() {
+	if config.isConfigAnyRequiredFieldEmpty() {
 		return nil, errors.New("config file is missing required fields")
 	}
 
 	return &config, nil
 }
 
-func ExtractValuesFromConfig(app *Application) (name string, url string, expectedStatusCode int, timeout time.Duration, expectedActualLocation string) {
-	name = app.Name
-	url = app.URL
-	expectedStatusCode = app.ExpectedStatusCode
-	timeout = app.Timeout
-	expectedActualLocation = app.ExpectedLocation
-	return
-}
-
-func ContainApp(applications []*Application, e string) bool {
+func ContainApp(applications []*a.Application, e string) bool {
 	for _, application := range applications {
 		if application.Name == e {
 			return true
