91da739d2c6c9798a992adfa99496af8106a0f9c
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-22 12:53:05 -0400

    helper.go: Add and refactor to test dev apps and posting messages to Slack dev & prod channels

--- helper.go
+++ helper.go
@@ -6,34 +6,42 @@ import (
 	a "github.com/NYULibraries/aswa/lib/application"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 // postTestResult posts the result of the given test to Slack.
-func postTestResult(test *a.Application, channel string, token string) error {
+func postTestResult(test *a.Application, channelProdId, channelDevId, token string) error {
 	appStatus := test.GetStatus()
 	log.Println(appStatus)
 
 	// check if the status is not successful
 	if !appStatus.Success {
+		// Determine the channel to post the message
+		var targetChannel string
+		if strings.HasPrefix(strings.ToLower(test.Name), "dev") {
+			targetChannel = channelDevId
+		} else {
+			targetChannel = channelProdId
+		}
+
 		slackClient := NewSlackClient(token)
-		if err := slackClient.PostToSlack(appStatus.String(), channel); err != nil {
+		if err := slackClient.PostToSlack(appStatus.String(), targetChannel); err != nil {
 			return err
 		}
 		timestamp := time.Now().Local().Format(time.RFC1123Z)
-		log.Printf("Message sent to channel %s on %s", channel, timestamp)
+		log.Printf("Message sent to channel %s on %s", targetChannel, timestamp)
 	}
 
 	return nil
 }
 
-func RunSyntheticTests(appData []*a.Application, channel string, token string, targetAppName string) error {
+func RunSyntheticTests(appData []*a.Application, channelProdId, channelDevId, token, targetAppName string) error {
 	found := false // Keep track of whether the app was found in the config file
 	for _, app := range appData {
-
 		if targetAppName == "" || targetAppName == app.Name {
 			found = true // The app was found in the config file
-			err := postTestResult(app, channel, token)
+			err := postTestResult(app, channelProdId, channelDevId, token)
 			if err != nil {
 				return err
 			}
@@ -53,29 +61,61 @@ func RunSyntheticTests(appData []*a.Application, channel string, token string, t
 	return nil
 }
 
-func getSlackCredentials() (string, string, error) {
-	channelId := os.Getenv(envSlackChannelId)
+func getSlackCredentials() (string, string, string, error) {
+	channelProdId := os.Getenv(envSlackChannelProdId)
+	channelDevId := os.Getenv(envSlackChannelDevId)
 	token := os.Getenv(envSlackToken)
-	if channelId == "" || token == "" {
-		if channelId == "" && token == "" {
-			// if both are not set, log a warning and return with no error
-			log.Println("SLACK_CHANNEL_ID and SLACK_TOKEN environment variables are not set")
-			return "", "", nil
-		}
-		// if only one of the variables is set, return an error
-		return "", "", errors.New("SLACK_CHANNEL_ID and SLACK_TOKEN environment variables must both be set")
+
+	missingVars := checkMissingSlackEnvVariables(channelProdId, channelDevId, token)
+
+	if len(missingVars) == 3 {
+		log.Println("SLACK_CHANNEL_Prod_ID, SLACK_CHANNEL_Dev_ID, and SLACK_TOKEN environment variables are not set")
+		return "", "", "", nil
+	} else if len(missingVars) > 0 {
+		errorMsg := createCustomSlackErrorMessage(missingVars)
+		return "", "", "", errors.New(errorMsg)
 	}
 
 	// Check if the credentials are valid by checking auth.test
 	err := ValidateSlackCredentials(token)
 	if err != nil {
-		return "", "", fmt.Errorf("invalid slack credentials: %v", err)
+		return "", "", "", fmt.Errorf("invalid slack credentials: %v", err)
 	}
 
-	return channelId, token, nil
+	return channelProdId, channelDevId, token, nil
+}
+
+func checkMissingSlackEnvVariables(channelProdId, channelDevId, token string) []string {
+	var missingSlackEnvVars []string
+
+	envVars := map[string]string{
+		"SLACK_CHANNEL_PROD_ID": channelProdId,
+		"SLACK_CHANNEL_DEV_ID":  channelDevId,
+		"SLACK_TOKEN":           token,
+	}
+
+	for key, value := range envVars {
+		if value == "" {
+			missingSlackEnvVars = append(missingSlackEnvVars, key)
+		}
+	}
+
+	return missingSlackEnvVars
+}
+
+func createCustomSlackErrorMessage(missingSlackEnvVars []string) string {
+	errorSlackMsg := "The following environment variables must be set: "
+	for i, missingSlackEnvVar := range missingSlackEnvVars {
+		if i > 0 {
+			errorSlackMsg += ", "
+		}
+		errorSlackMsg += missingSlackEnvVar
+	}
+	return errorSlackMsg
 }
 
 const (
-	envSlackChannelId = "SLACK_CHANNEL_ID"
-	envSlackToken     = "SLACK_TOKEN"
+	envSlackChannelProdId = "SLACK_CHANNEL_PROD_ID"
+	envSlackChannelDevId  = "SLACK_CHANNEL_DEV_ID"
+	envSlackToken         = "SLACK_TOKEN"
 )
