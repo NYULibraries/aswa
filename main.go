7147b341af0da45ae19a3c6df4ab61d80c97a505
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-04-28 11:40:48 -0400

    Refactor main function and create Check struct

--- main.go
+++ main.go
@@ -1,44 +1,12 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	c "github.com/NYULibraries/aswa/lib/config"
-)
-
 func main() {
-	logger := log.New(os.Stdout, "", 0)
+	// Call the Do() method on the Check instance and retrieve any errors.
+	err := check.Do()
 
-	yamlPath := os.Getenv(envYamlPath)
-	if yamlPath == "" {
-		logger.Println("WARNING: Environment variable YAML_PATH is not set")
-	}
-
-	inputData, err := c.NewConfig(yamlPath)
+	// If an error occurs during the execution of the Do() method,
+	// log the error using the Logger stored in the Check instance and terminate the program.
 	if err != nil {
-		logger.Fatal("Could not load config file; aborting!", err)
-	}
-
-	appData := inputData.Applications
-
-	var channelId, token, errorSlack = getSlackCredentials()
-
-	if errorSlack != nil {
-		logger.Fatal("Error checking Slack environment variables:", errorSlack)
-	}
-	// Get the command line argument without using the flag package
-	var cmdArg string
-	if len(os.Args) == 1 {
-		cmdArg = ""
-	} else {
-		cmdArg = os.Args[1]
-	}
-
-	err = RunSyntheticTests(appData, channelId, token, cmdArg)
-	if err != nil {
-		logger.Fatal("Error running tests:", err)
+		check.Logger.Fatal("Error:", err)
 	}
 }
-
-const envYamlPath = "YAML_PATH"
