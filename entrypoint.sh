4834a0ad3e0fbe9cc441111cdbb49c07567967c3
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-09-01 17:00:26 -0400

    entrypoint.sh: Add default value to cluster name if CLUSTER_INFO is not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ aswa_output=$(if ! command -v "$1" >/dev/null 2>&1; then /aswa "$1"; else "$@" &
 echo "aswa_output: ${aswa_output}"
 
 if echo "$aswa_output" | grep -q "Failure"; then
-    cluster_name=$CLUSTER_INFO
-    slack_message="Cluster ${cluster_name}: ${aswa_output}"
+    cluster_name=${CLUSTER_INFO:-"Unknown cluster:CLUSTER_INFO is not set"}
+    slack_message="${cluster_name}: ${aswa_output}"
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${slack_message}\"}" "${SLACK_WEBHOOK_URL}"
 fi
 
