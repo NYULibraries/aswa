b8c313fbd39566900393c70b319b410f58c53d21
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-09-05 10:39:03 -0400

    entrypoint.sh: Update shell script to use jq for constructing Slack payload; Improve error handling and debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-# Capture the output of /aswa
-aswa_output=$(if ! command -v "$1" >/dev/null 2>&1; then /aswa "$1"; else "$@" & fi)
+# Debug: Print SLACK_WEBHOOK_URL
+echo "SLACK_WEBHOOK_URL: ${SLACK_WEBHOOK_URL}"
 
+# Capture the output of /aswa
+aswa_output=$(if ! command -v "$1" >/dev/null 2>&1; then /aswa "$1"; else "$@"; fi)
+
+# Debug: Print aswa_output
 echo "aswa_output: ${aswa_output}"
 
 if echo "$aswa_output" | grep -q "Failure"; then
     cluster_name=${CLUSTER_INFO:-"Unknown cluster:CLUSTER_INFO is not set"}
     slack_message="${cluster_name}: ${aswa_output}"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${slack_message}\"}" "${SLACK_WEBHOOK_URL}"
+
+    # Debug: Print slack_message
+    echo "slack_message: ${slack_message}"
+
+    # Construct JSON payload using jq
+    payload=$(echo '{}' | jq --arg text "$slack_message" '.text = $text')
+
+    curl -X POST -H 'Content-type: application/json' --data "$payload" "${SLACK_WEBHOOK_URL}"
 fi
-
-
-
