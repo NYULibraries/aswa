54002190d0e227712496659644c1eb532b3f09d5
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2025-10-30 16:14:32 -0400

    Remove unused log import from metrics.go

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,8 +1,6 @@
 package metrics
 
 import (
-	"log"
-
 	c "github.com/NYULibraries/aswa/pkg/config"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
